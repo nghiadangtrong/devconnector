994771a3761bf9e4868fdd6e2c7104694b0878f8
noan <nghiadangtrongn92@gmail.com>
2020-03-20 01:23:13 +0700

    add server routes

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // mongoURI: process.env.MONGO_URI
+    mongoURI: "mongodb+srv://noan:myRbs4W2EUvdWJRl@cluster0-qqqga.mongodb.net/db_devconnector?retryWrites=true&w=majority"
+}
